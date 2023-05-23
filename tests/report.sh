abd0f4ab6c5e1326200572a2ba972d146db7198d
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-24 02:13:36 +0530

    Updating test script

--- tests/report.sh
+++ tests/report.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-Type: application/json" -d '{"url":"techcrunch.com"}' http://localhost:3000/api/report -o ./output/techcrunch.json
+curl -X POST -H "Content-Type: application/json" -d '{"url":"techcrunch.com"}' http://localhost:3000/api/report -o ./output/techcrunch-total.json
