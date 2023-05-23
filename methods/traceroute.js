98398b6cb7cd11be99104b70661f7a902b8f9311
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-23 15:53:35 +0530

    Completing Tracerouting + Geolocation module

--- methods/traceroute.js
+++ methods/traceroute.js
@@ -40,9 +40,19 @@ const geolocateMaxmind = async (ip) => {
 
   // EXECUTING GEOLOCATION
   try {
-    const response = await client.city(ip)
+    const geolocationData = await client.city(ip)
+    if (geolocationData.location === undefined) throw 'CANNOT BE GEOLOCATED'
+    else {
+      return {
+        country: geolocationData.country?.names.en.toUpperCase(),
+        city: geolocationData.city?.names.en.toUpperCase(),
+        postalCode: geolocationData.postal?.code,
+        latitude: geolocationData.location.latitude,
+        longitude: geolocationData.location.longitude,
+      }
+    }
   } catch (err) {
-    console.log(`ERR (GEOLOCATION/PANGEA): ${err.msg}`)
+    console.log(`ERR (GEOLOCATION/PANGEA): `, err.code)
     return null
   }
 }
@@ -72,9 +82,47 @@ const geolocatePangea = async (ip) => {
   // EXECUTING GEOLOCATION
   try {
     const response = await fetch(url, options)
+    const geolocationData = await response.json()
+    if (geolocationData.status.toUpperCase() === 'SUCCESS') {
+      return {
+        country: geolocationData.result.data.country.toUpperCase(),
+        city: geolocationData.result.data.city.toUpperCase(),
+        postalCode: geolocationData.result.data.postal_code,
+        latitude: geolocationData.result.data.latitude,
+        longitude: geolocationData.result.data.country.longitude,
+      }
+    } else throw 'CANNOT BE GEOLOCATED'
   } catch (err) {
-    console.log(`ERR (GEOLOCATION/MAXMIND): ${err.msg}`)
+    console.log(`ERR (GEOLOCATION/MAXMIND): `, err)
     return null
+  }
+}
+
+/**
+ * @name getGeolocation
+ * @description METHOD TO GET GEOLOCATION
+ * @param {*} ip IP ADDRESS
+ * @returns {Object} GEOLOCATION DATA
+ */
+const getGeolocation = async (ip) => {
+  const maxmindRes = await geolocateMaxmind(ip)
+  if (maxmindRes !== null) {
+    return {
+      type: 'GEOLOCATED',
+      data: maxmindRes,
+    }
+  } else {
+    const pangeaRes = await geolocatePangea(ip)
+    if (pangeaRes !== null) {
+      return {
+        type: 'GEOLOCATED',
+        data: pangeaRes,
+      }
+    } else {
+      return {
+        type: 'UNGEOLOCATED',
+      }
+    }
   }
 }
 
@@ -87,7 +135,7 @@ const geolocatePangea = async (ip) => {
 const traceroute = async (url) => {
   return new Promise((resolve, reject) => {
     // STORING TRACEROUTING DATA
-    const TracerouteData = {
+    let TracerouteData = {
       destination: null,
       hops: [],
       closeCode: null,
@@ -109,17 +157,47 @@ const traceroute = async (url) => {
         else if (isPrivateIP(hop.ip))
           TracerouteData.hops.push({ ...hop, type: 'PRIVATE' })
         // PUBLIC IP ADDRESS
-        else TracerouteData.hops.push({ ...hop, type: 'PUBLIC' })
+        else {
+          TracerouteData.hops.push({ ...hop, type: 'PUBLIC' })
+        }
       })
-      .on('close', (code) => {
-        // TODO: SETTING FIRST HOP DATA BASED ON PREDEFINED DATA AS ORIGIN LOCATION IS KNOWN
-        // SETTING LAST HOP DATA AS DESTINATION IP => IN SOME CASES IT RETURNS AS *.*.*.*
-        TracerouteData.hops[TracerouteData.hops.length - 1]['ip'] =
-          TracerouteData.destination
+      .on('close', async (code) => {
+        // SETTING INITIAL HOPS AS THE ORIGIN IP
+        TracerouteData.hops.unshift({
+          hop: 0,
+          ip: process.env.DEVICE_IP,
+          rtt: '0 ms',
+          type: 'PUBLIC',
+        })
 
-        // RESOLVING REQUEST
+        // SETTING LAST HOP DATA AS DESTINATION IP => IN SOME CASES IT RETURNS AS *.*.*.*
+        TracerouteData.hops[TracerouteData.hops.length - 1] = {
+          ...TracerouteData.hops[TracerouteData.hops.length - 1],
+          ip: TracerouteData.destination,
+          type: 'PUBLIC',
+        }
+
         console.log(`TRACEROUTE CLOSE CODE: ${code}`)
         TracerouteData.closeCode = code
+
+        // GETTING GEOLOCATION DATA FOR HOPS
+        TracerouteData = {
+          ...TracerouteData,
+          hops: await Promise.all(
+            TracerouteData.hops.map(async (hop) => {
+              if (hop.type === 'PUBLIC') {
+                console.log(`GEOLOCATING FOR ${hop.ip}`)
+                const geolocationData = await getGeolocation(hop.ip)
+                return {
+                  ...hop,
+                  ...geolocationData,
+                }
+              } else return hop
+            })
+          ),
+        }
+
+        // RESOLVING REQUEST
         resolve(TracerouteData)
       })
       .on('error', (error) => {
