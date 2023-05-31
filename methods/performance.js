c8b620afc1dd0b582d7b735166d6ec6a087fcb67
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-31 18:04:13 +0530

    Extracting site metadata

--- methods/performance.js
+++ methods/performance.js
@@ -39,34 +39,81 @@ const runLighthouse = async (url) => {
 const transformPerformanceData = (PerformanceData) => {
   console.log('TRANSFORMING PERFORMANCE DATA')
 
+  // STORING SITE METADATA
+  const description = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.name === 'description'
+  )
+  const ogUrl = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'og:url'
+  )
+  const ogTitle = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'og:title'
+  )
+  const ogDescription = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'og:description'
+  )
+  const ogSiteName = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'og:site_name'
+  )
+  const ogName = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'og:name'
+  )
+  const ogImage = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'og:image'
+  )
+  const ogImageAlt = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'og:image:alt'
+  )
+  const twitterTitle = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'twitter:title'
+  )
+  const twitterDescription = PerformanceData.artifacts.MetaElements.filter(
+    (MetaElement) => MetaElement.property === 'twitter:description'
+  )
+  const siteMeta = {
+    ogUrl: description.length !== 0 ? description[0].content : null,
+    ogUrl: ogUrl.length !== 0 ? ogUrl[0].content : null,
+    ogTitle: ogTitle.length !== 0 ? ogTitle[0].content : null,
+    ogTitle: ogDescription.length !== 0 ? ogDescription[0].content : null,
+    ogSiteName: ogSiteName.length !== 0 ? ogSiteName[0].content : null,
+    ogName: ogName.length !== 0 ? ogName[0].content : null,
+    ogImage: ogImage.length !== 0 ? ogImage[0].content : null,
+    ogImageAlt: ogImageAlt.length !== 0 ? ogImageAlt[0].content : null,
+    twitterTitle: twitterTitle.length !== 0 ? twitterTitle[0].content : null,
+    twitterDescription:
+      twitterDescription.length !== 0 ? twitterDescription[0].content : null,
+  }
+
   return {
-    lhVersion: PerformanceData.lhr.lighthouseVersion,
-    url: PerformanceData.lhr.requestedUrl,
-    finalUrl: PerformanceData.lhr.finalUrl,
-    fcpScore: PerformanceData.lhr.audits['first-contentful-paint'].score,
-    fcpValue: PerformanceData.lhr.audits['first-contentful-paint'].numericValue,
-    lcpScore: PerformanceData.lhr.audits['largest-contentful-paint'].score,
+    lhVersion: PerformanceData.lhr?.lighthouseVersion,
+    url: PerformanceData.lhr?.requestedUrl,
+    finalUrl: PerformanceData.lhr?.finalUrl,
+    fcpScore: PerformanceData.lhr?.audits['first-contentful-paint'].score,
+    fcpValue:
+      PerformanceData.lhr?.audits['first-contentful-paint'].numericValue,
+    lcpScore: PerformanceData.lhr?.audits['largest-contentful-paint'].score,
     lcpValue:
-      PerformanceData.lhr.audits['largest-contentful-paint'].numericValue,
-    tbtScore: PerformanceData.lhr.audits['total-blocking-time'].score,
-    tbtValue: PerformanceData.lhr.audits['total-blocking-time'].numericValue,
-    ttiScore: PerformanceData.lhr.audits['interactive'].score,
-    ttiValue: PerformanceData.lhr.audits['interactive'].numericValue,
+      PerformanceData.lhr?.audits['largest-contentful-paint'].numericValue,
+    tbtScore: PerformanceData.lhr?.audits['total-blocking-time'].score,
+    tbtValue: PerformanceData.lhr?.audits['total-blocking-time'].numericValue,
+    ttiScore: PerformanceData.lhr?.audits['interactive'].score,
+    ttiValue: PerformanceData.lhr?.audits['interactive'].numericValue,
     clsScore:
-      PerformanceData.lhr.audits['cumulative-layout-shift'].numericValue,
-    srtValue: PerformanceData.lhr.audits['server-response-time'].numericValue,
-    srtItems: PerformanceData.lhr.audits['server-response-time'].details.items,
-    speedIndexScore: PerformanceData.lhr.audits['speed-index'].score,
-    speedIndexValue: PerformanceData.lhr.audits['speed-index'].numericValue,
-    bootupTimeScore: PerformanceData.lhr.audits['bootup-time'].score,
-    bootupTimeValue: PerformanceData.lhr.audits['bootup-time'].numericValue,
-    bootupTimeItems: PerformanceData.lhr.audits['bootup-time'].details.items,
+      PerformanceData.lhr?.audits['cumulative-layout-shift'].numericValue,
+    srtValue: PerformanceData.lhr?.audits['server-response-time'].numericValue,
+    srtItems: PerformanceData.lhr?.audits['server-response-time'].details.items,
+    speedIndexScore: PerformanceData.lhr?.audits['speed-index'].score,
+    speedIndexValue: PerformanceData.lhr?.audits['speed-index'].numericValue,
+    bootupTimeScore: PerformanceData.lhr?.audits['bootup-time'].score,
+    bootupTimeValue: PerformanceData.lhr?.audits['bootup-time'].numericValue,
+    bootupTimeItems: PerformanceData.lhr?.audits['bootup-time'].details.items,
     bootupTimeSummary:
-      PerformanceData.lhr.audits['bootup-time'].details.summary,
+      PerformanceData.lhr?.audits['bootup-time'].details.summary,
     thirdPartyItems:
-      PerformanceData.lhr.audits['third-party-summary'].details.items,
+      PerformanceData.lhr?.audits['third-party-summary'].details.items,
     thirdPartySummary:
-      PerformanceData.lhr.audits['third-party-summary'].details.summary,
+      PerformanceData.lhr?.audits['third-party-summary'].details.summary,
+    siteMeta: siteMeta,
   }
 }
 
